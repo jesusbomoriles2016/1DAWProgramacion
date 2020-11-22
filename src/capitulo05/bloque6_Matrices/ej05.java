db8fae6e04557d6750d895f9286323d7ad9c43d8
jesusbomoriles2016 <72796727+jesusbomoriles2016@users.noreply.github.com>
2020-11-22 19:01:18 +0100

    22/11/2020

--- src/capitulo05/bloque6_Matrices/ej05.java
+++ src/capitulo05/bloque6_Matrices/ej05.java
@@ -2,8 +2,53 @@ package capitulo05.bloque6_Matrices;
 
 public class ej05 {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		int matriz [][] = new int [][] { {8, 8, 8},
+										{0, 6, 9},
+										{0, 0, 4}};
 		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println("");
+		}	
+		
+		System.out.println("La matriz es triangular superior: " + matrizEsTriangularSuperior(matriz));
+		
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param matriz
+	 * @return
+	 */
+	public static boolean matrizEsTriangularSuperior (int matriz [][]) {
+		boolean esTriangularSuperior = false;
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == i) {
+					if (matriz[i][j] != 0 && matriz[i][j-1] == 0 && matriz[i][j+1] == 0) {
+						esTriangularSuperior = true;
+					}
+				
+				}
+				
+				
+				
+				
+			}
+			
+		}
+			
+		return esTriangularSuperior;
 	}
 
 }
