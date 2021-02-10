576bce58ba5bfb3171ae0c3bbdb92ca4b58b40f3
jesusbomoriles2016 <72796727+jesusbomoriles2016@users.noreply.github.com>
2021-02-10 11:55:19 +0100

    10/02/2020 11:55

--- src/capitulo02/Ejercicios_capitulo02_bloque03.java
+++ src/capitulo02/Ejercicios_capitulo02_bloque03.java
@@ -57,25 +57,25 @@ public class Ejercicios_capitulo02_bloque03 {
 		int elegirOperacion, numRaiz = 0, basePotencia, exponente = 0, resultadoPotencia, dividendo, divisor, modDivision = 0;
 		double resultadoRaiz;
 		System.out.println("Bienvenido a la calculadora avanzada: ");
-		System.out.println("Pulsa 1 para realizar una raíz.");
+		System.out.println("Pulsa 1 para realizar una raï¿½z.");
 		System.out.println("Pulsa 2 para realizar una potencia.");
-		System.out.println("Pulsa 3 para realizar el módulo de una división.");
-		System.out.println("Pulsa cualquier otro número para terminar.");
+		System.out.println("Pulsa 3 para realizar el mï¿½dulo de una divisiï¿½n.");
+		System.out.println("Pulsa cualquier otro nï¿½mero para terminar.");
 		
-		String str = JOptionPane.showInputDialog("¿Qué quieres hacer?");
+		String str = JOptionPane.showInputDialog("ï¿½Quï¿½ quieres hacer?");
 		elegirOperacion= Integer.parseInt(str);
 		
 		if (elegirOperacion == 1) {
-			String str1 = JOptionPane.showInputDialog("Introduce un número");
+			String str1 = JOptionPane.showInputDialog("Introduce un nï¿½mero");
 			numRaiz = Integer.parseInt(str1);
 			resultadoRaiz = Math.sqrt(numRaiz);
 			
-			System.out.println("La raíz de " + numRaiz + " es " + resultadoRaiz);
+			System.out.println("La raï¿½z de " + numRaiz + " es " + resultadoRaiz);
 			System.out.println("El programa ha terminado.");
 		}
 		
 		else if (elegirOperacion == 2) {
-			String str2 = JOptionPane.showInputDialog("Introduce un número ");
+			String str2 = JOptionPane.showInputDialog("Introduce un nï¿½mero ");
 			basePotencia = Integer.parseInt(str2);
 			String str3 = JOptionPane.showInputDialog("Introduce el exponente: ");
 			exponente = Integer.parseInt(str3);
@@ -92,7 +92,7 @@ public class Ejercicios_capitulo02_bloque03 {
 			divisor = Integer.parseInt(str5);
 			modDivision = dividendo % divisor;
 			
-			System.out.println("El módulo de la división con dividendo " + dividendo + " y divisor " + divisor + " es: " + modDivision);
+			System.out.println("El mï¿½dulo de la divisiï¿½n con dividendo " + dividendo + " y divisor " + divisor + " es: " + modDivision);
 			System.out.println("El programa ha terminado.");
 		}
 		
@@ -103,17 +103,17 @@ public class Ejercicios_capitulo02_bloque03 {
 	}
 	
 	public static void Ejercicio3 () {
-		int elegirOperacion, longCateto1, longCateto2, longRadioCircunferencia, baseRectángulo, alturaRectángulo, baseTriángulo, alturaTriángulo;
+		int elegirOperacion, longCateto1, longCateto2, longRadioCircunferencia, baseRectangulo, alturaRectangulo, baseTriangulo, alturaTriangulo;
 		
-		System.out.println("MENÚ DE CÁLCULO (pulsa para ejecutar):");
-		System.out.println("1. Cálculo de la hipotenusa de un triángulo.");
-		System.out.println("2. Cálculo de la superficie de una circunferencia.");
-		System.out.println("3. Cálculo del perímetro de una circunferencia.");
-		System.out.println("4. Cálculo del área de un rectángulo.");
-		System.out.println("5. Cálculo del área de un triángulo.");
-		System.out.println("Salir de la aplicación.");
+		System.out.println("MENï¿½ DE Cï¿½LCULO (pulsa para ejecutar):");
+		System.out.println("1. Cï¿½lculo de la hipotenusa de un triï¿½ngulo.");
+		System.out.println("2. Cï¿½lculo de la superficie de una circunferencia.");
+		System.out.println("3. Cï¿½lculo del perï¿½metro de una circunferencia.");
+		System.out.println("4. Cï¿½lculo del ï¿½rea de un rectï¿½ngulo.");
+		System.out.println("5. Cï¿½lculo del ï¿½rea de un triï¿½ngulo.");
+		System.out.println("Salir de la aplicaciï¿½n.");
 		
-		String str = JOptionPane.showInputDialog("Introduce un número: ");
+		String str = JOptionPane.showInputDialog("Introduce un nï¿½mero: ");
 		elegirOperacion = Integer.parseInt(str);
 		
 		if (elegirOperacion != 0) {
@@ -147,24 +147,24 @@ public class Ejercicios_capitulo02_bloque03 {
 			
 			else if (elegirOperacion == 4) {
 				
-				String str5 = JOptionPane.showInputDialog("Introduce la longitud de la base del rectángulo: ");
-				baseRectángulo = Integer.parseInt(str5);
+				String str5 = JOptionPane.showInputDialog("Introduce la longitud de la base del rectangulo: ");
+				baseRectangulo = Integer.parseInt(str5);
 				
-				String str6 = JOptionPane.showInputDialog("Introduce la longitud de la altura del rectágulo: ");
-				alturaRectángulo = Integer.parseInt(str6);
+				String str6 = JOptionPane.showInputDialog("Introduce la longitud de la altura del rectagulo: ");
+				alturaRectangulo = Integer.parseInt(str6);
 				
-				System.out.println("El área del rectángulo es: " + baseRectángulo * alturaRectángulo);
+				System.out.println("El ï¿½rea del rectï¿½ngulo es: " + baseRectï¿½ngulo * alturaRectï¿½ngulo);
 			}
 			
 			else if (elegirOperacion == 5) {
 				
-				String str7 = JOptionPane.showInputDialog("Introduce la base del triángulo: ");
-				baseTriángulo = Integer.parseInt(str7);
+				String str7 = JOptionPane.showInputDialog("Introduce la base del triï¿½ngulo: ");
+				baseTriï¿½ngulo = Integer.parseInt(str7);
 				
-				String str8 = JOptionPane.showInputDialog("Introduce la altura del triángulo: ");
-				alturaTriángulo = Integer.parseInt(str8);
+				String str8 = JOptionPane.showInputDialog("Introduce la altura del triï¿½ngulo: ");
+				alturaTriï¿½ngulo = Integer.parseInt(str8);
 				
-				System.out.println("El área del triángulo es: " + (baseTriángulo * alturaTriángulo) / 2);
+				System.out.println("El ï¿½rea del triï¿½ngulo es: " + (baseTriï¿½ngulo * alturaTriï¿½ngulo) / 2);
 			}
 			
 			else if (elegirOperacion == 0) {
@@ -172,7 +172,7 @@ public class Ejercicios_capitulo02_bloque03 {
 			}
 			
 			else {
-				System.out.println("Opción incorrecta");
+				System.out.println("Opciï¿½n incorrecta");
 			}
 			
 		}
